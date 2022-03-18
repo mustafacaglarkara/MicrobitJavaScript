e97a7b25dc18d2b94ec66e65c6010932cde72514
Mustafa Çağlar KARA <mustafacaglarkara@gmail.com>
2022-03-18 12:30:27 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+let ad = 'Mustafa'; //string 
+let soyad = 'KARA'; //string
+let yas = 38; // number int 
+let egitmen_mi = true; // boolean
+let maas = 4.500; // number
+let kisi = {
+    ad : "Mustafa",
+    soyad: "KARA",
+    yas : 38
+} //object 
+
+
+console.log(typeof(kisi));
